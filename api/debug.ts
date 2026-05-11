e8461d387b8fad6ff75b69559e43868816d82536
mohamedaidal60 <mohamedaidal60@gmail.com>
2026-05-11 21:33:29 +0200

    API: More debug info.

--- api/debug.ts
+++ api/debug.ts
@@ -1,7 +1,7 @@
 export default function (req: any, res: any) {
   res.json({
     hasDbUrl: !!process.env.DATABASE_URL,
-    dbUrlPrefix: process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 15) : 'none',
+    dbUrlPrefix: process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 40) : 'none',
     nodeEnv: process.env.NODE_ENV,
     time: new Date().toISOString()
   });
